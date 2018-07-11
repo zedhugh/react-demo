a76276b572502d0873affd15f4b618a7e7708af6
Zedhugh Chen <zedhugh@gmail.com>
2018-07-12 02:07:49 +0800

    add example for Refs.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Hello extends Component {
+class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      const node = this.myRef.current;
+      fetch("http://zedhugh.me/config")
+        .then(r => r.text())
+        .then((d) => {
+          node.textContent = d;
+        });
+    }, 1000);
+  }
+
   render() {
     return (
-      <div>
-        Hello React
+      <div ref={this.myRef}>
+        Good
       </div>
     );
   }

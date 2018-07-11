c150fe53d6fc6be4292f7f5631f3338e2e304a31
Zedhugh Chen <zedhugh@gmail.com>
2018-07-11 16:39:29 +0800

    add a webpack config file for production.

---
+++ webpack.product.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: [
+    path.join(__dirname, 'src/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJSPlugin()
+  ]
+};

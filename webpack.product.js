ba53748c1a1658ba35ee76f8fd37f0a6e0842197
chenzh <605691129@qq.com>
2019-05-09 13:20:27 +0800

    add ".editorconfig" file.

--- webpack.product.js
+++ webpack.product.js
@@ -21,8 +21,8 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader']
       }, {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader?modules']
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader?modules']
       }
     ]
   },
